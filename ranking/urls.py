8fc43fc50ad8028e577e9918643a9ac5e0bcc9d0
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2026-02-02 21:47:28 +0200

    feat(api):implement get my rank endpoint

--- ranking/urls.py
+++ ranking/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SubmitScoreView,TopPlayersView
+from .views import SubmitScoreView,TopPlayersView,MyRankView
 
 urlpatterns = [
     path('score/', SubmitScoreView.as_view(), name='submit-score'),
     path('top-players/', TopPlayersView.as_view(), name='top-players'),
+    path('my-rank/', MyRankView.as_view(), name='my-rank'),
 ]
--- ranking/views.py
+++ ranking/views.py
@@ -17,3 +17,22 @@ class TopPlayersView(generics.ListAPIView):
     def get_queryset(self):
         return Score.objects.select_related('player').order_by('-points')[:10]
     
+
+class MyRankView(views.APIView):
+    def get(self,request):
+        username=request.query_params.get('username')
+        if not username:
+            return Response({"error":"Username query parameter is required."},status=status.HTTP_400_BAD_REQUEST)
+        
+        player=get_object_or_404(Player,username=username)
+        player_scores=Score.objects.filter(player=player).aggregate(max_points=Max('points'))
+        high_score = player_scores['max_points']
+
+        if high_score is None:
+            return Response({"message": "Player not found or has no scores yet"}, 
+                status=status.HTTP_404_NOT_FOUND)
+
+        better_players_count=Score.objects.values('player').annotate(max_points=Max('points')).filter(max_points__gt=high_score).count()
+        rank=better_players_count+1
+
+        return Response({"username":username,"high_score":high_score,"rank":rank},status=status.HTTP_200_OK)
